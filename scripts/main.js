94f5b055bdd7b391832fa09c052f6fad79188353
Nathan <shep.nathan.d@gmail.com>
2019-11-13 15:23:30 -0500

    added styling to navbar

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
         $(window).scrollTop($(document.getElementById($(event.target).text().replace(/ /g,"-").replace(/\?/g,"").replace(/\'/g,"").toLowerCase())).offset().top);
 		window.scrollBy(0,-60);
     });
+
+    $(function() {
+    	var path = location.pathname;
+    	$("a[href='" + path + "']").addClass('active');
+	})
 });
